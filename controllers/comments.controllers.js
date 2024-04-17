f2132dbc78fbf677d8f890f1f41c419818642492
mikeold-2008 <mikeold2008@gmail.com>
2024-04-17 11:12:49 +0100

    adds 9-delete-comment

--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -1,10 +1,9 @@
 const app = require("../app")
-const {fetchCommentsByArticleId,insertComments} = require("../models/comments.models")
+const {fetchCommentsByArticleId,insertComments,removeCommentById,checkCommentExists} = require("../models/comments.models")
 const {checkArticleExists} = require("../models/articles.models")
 
 function getCommentsByArticleId(req,res,next){
     const {article_id} = req.params
-
 
     Promise.all([fetchCommentsByArticleId(article_id),checkArticleExists(article_id)])
     .then(([comments]) => {
@@ -12,7 +11,6 @@ function getCommentsByArticleId(req,res,next){
     })
     .catch(next)
 }
-
 
 
 function postComments(req,res,next){
@@ -31,4 +29,20 @@ function postComments(req,res,next){
 }
 
 
-module.exports = {getCommentsByArticleId, postComments}
+function deleteCommentById(req,res,next){
+    const {comment_id} = req.params
+
+    checkCommentExists(comment_id)
+    .then(
+    removeCommentById(comment_id)
+    .then((comment) => {
+        res.status(204).send({})
+    })
+    .catch((err) => {
+        next(err)
+    })
+    ).catch(next)
+}
+
+
+module.exports = {getCommentsByArticleId, postComments,deleteCommentById}
